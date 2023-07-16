b764afdfbf5fd912b1aba0b74db13be6e0759ee6
Khachkara <khachkara@gmail.com>
2023-07-16 11:50:06 +0300

    delete txt file
    add link to youtube playlist

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Dynamic-Programming
 Video course of Dynamic Programming by Andrey Grehov
+GitHub: hhttps://github.com/andreygrehov/dp/blob/master/lecture4/lecture4.go
+LinkedIn: https://www.linkedin.com/in/andrey-grehov/
+Twitter: https://twitter.com/andreygrehov
+youtube: https://youtube.com/playlist?list=PLVrpF4r7WIhTT1hJqZmjP10nxsmrbRvlf
--- lecture5_part2.py
+++ lecture5_part2.py
@@ -7,27 +7,26 @@ Problem:
 
 Framework for Solving DP Problems:
 	1. Define the objective function
-		f(i) is the number of distinct ways to reach the i-th stair.
+		f(i) is the number of distinct ways to reach the i-th stair by making 1 to k stairs.
 	2. Identify base cases
 		f(0) = 1
 		f(1) = 1
 	3. Write down a recurrence relation for the optimized objective function
-		f(n) = f(n-1) + f(n-2)
+		f(n) = f(n-1) + f(n-2) + ... + f(n - k)
 	4. What's the order of execution?
 		bottom-up
 	5. Where to look for the answer?
 		f(n)
 '''
-def climbingStaors(n):
+def climbingStaors(n, k):
     dp = [0] * (n + 1)
     dp[0] = 1
     dp[1] = 1
-    k = 3
     for i in range(2, n + 1):
-		# dp[i] = dp[i - 1] + dp[i - 2]  # k = 2
+		# dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3] + dp[i - k]  # k = 2
         for j in range(k):
             if i - j < 0:
                 continue
             dp[i] += dp[i - j]
     return dp[n]
-print(climbingStaors(6))
+print(climbingStaors(2, 3))
