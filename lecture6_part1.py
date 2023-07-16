4958140b9bd3547445036aafa445a34f160c34f1
Khachkara <khachkara@gmail.com>
2023-07-16 13:00:22 +0300

    !

--- lecture6_part1.py
+++ lecture6_part1.py
@@ -6,14 +6,18 @@ Problem:
 	Each time you can climb 1..k steps. You are not allowed to step on red stairs.
 	In how many distinct ways can you climb to the top?
 '''
-def climbingStaors(n, k):
+def climbingStaors(n, k, stairs):
     dp = [0] * (k)
     dp[0] = 1
     dp[1] = 1
+    stairs = [1, 0, 1, 0, 0, 1]
     for i in range(1, n + 1):
         for j in range(k):
             if i - j < 0:
                 continue
-            dp[i % k]  += dp[(i - j) % k]
+            if stairs[i - 1]:
+                dp[i % k] = 0
+            else:   
+                dp[i % k]  += dp[(i - j) % k]
     return dp[n % k]
 print(climbingStaors(5, 3))
