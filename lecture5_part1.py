420f86d2481d1f059abcd76452de7373abbeefec
Khachkara <khachkara@gmail.com>
2023-07-16 10:15:04 +0300

    Intendention error

--- lecture5_part1.py
+++ lecture5_part1.py
@@ -19,13 +19,14 @@ Framework for Solving DP Problems:
 	5. Where to look for the answer?
 		f(n)
 '''
+
 def climbingStaors(n):
 	a = 1
 	b = 1
-    c = 0
-    for i in range(2, n + 1):
-        c = a + b
-        a = b
-        b = c
+	c = 0
+	for i in range(2, n + 1):
+		c = a + b
+		a = b
+		b = c
 	return c
 print(climbingStaors(6))
